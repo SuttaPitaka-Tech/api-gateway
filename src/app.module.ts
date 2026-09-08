6d8a71ff3abf48e5ef63eeaeecc9eb180f5b540a
Punithu <punithumesh2003@gmail.com>
2026-09-08 18:08:56 +0530

    added connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GatewayController } from './gateway.controller.js';
-import { APP_GUARD } from '@nestjs/core';
-import { KeycloakConnectModule, ResourceGuard, RoleGuard, AuthGuard } from 'nest-keycloak-connect';
 import { MinioModule } from './minio/minio.module';
 
 @Module({
@@ -11,28 +9,9 @@ import { MinioModule } from './minio/minio.module';
       isGlobal: true,
       envFilePath: 'environment/.env.local',
     }),
-    KeycloakConnectModule.register({
-      authServerUrl: process.env.KEYCLOAK_AUTH_SERVER_URL,
-      realm: process.env.KEYCLOAK_REALM,
-      clientId: process.env.KEYCLOAK_CLIENT_ID,
-      secret: process.env.KEYCLOAK_SECRET || '', 
-    }),
     MinioModule,
   ],
   controllers: [GatewayController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: ResourceGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RoleGuard,
-    },
-  ],
+  providers: [],
 })
 export class AppModule {}
